9100be9bc534d3e69a6c013de85ada76fbb81158
jjara1974 <jjara1974@gmail.com>
2020-10-02 17:30:03 +0200

    20201002

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormciComponent } from './formci/formci.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: FormciComponent },
+ //{ path: 'about', component: AboutComponent },
+ // { path: 'products', component: ProductsComponent },
+ // { path: 'product-detail/:id', component: ProductDetailComponent },  
+ // { path: 'contact', component: ContactComponent },
+ // { path: '**', component: NoPageFoundComponent }
 
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/formci/formci.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';  
+
+@Component({
+  selector: 'app-formci',
+  templateUrl: './formci.component.html',
+  styleUrls: ['./formci.component.css']
+})
+export class FormciComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
